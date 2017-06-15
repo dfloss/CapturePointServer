2fba27b6636b495c725bc85b9829a9c4ba6ad6be
Daniel Harris <harris.daniel.g@gmail.com>
2017-06-15 13:13:11 -0500

    added testing framework

---
+++ app/controller/games.js
@@ -0,0 +1,5 @@
+module.exports = function (models, config){
+    var games = {
+    }
+    return games
+}
---
+++ test/controller.teams.test.js
@@ -0,0 +1,21 @@
+var expect = require("chai").expect;
+var games = require("../app/controller/teams");
+var config = require("../config/config.json");
+
+describe("Team Creation",function(){
+    it("Will create a new team with a unique name",function(){
+        expect(true).to.equal(false);
+    });
+    it("Will block non-unique team names",function(){
+        expect(true).to.equal(false);
+    });
+});
+describe("Team ")
+describe("Team modification",function(){
+    it("Can change a team",function(){
+        expect(true).to.equal(false);
+    });
+    it("Will prevent changing a team name to a duplicate name",function(){
+
+    });
+});
