331cf4a8929a8d1f3fe746e4797a4ad1df1a5da0
Daniel Harris <dgharri@uark.edu>
2017-07-09 18:26:18 -0500

    controller work

--- app/controller/game.js
+++ app/controller/game.js
@@ -1,5 +1,22 @@
 module.exports = function (events, models, config){
     var game = {
+        start: (name,start,end,teamId) =>{
+            conflicts = models.Game.getConflicts(starttime,endTime);
+            if (conflicts == null){
+                var game = {
+                    name: name,
+                    start: starttime,
+                    end: end,
+                    teamId: teamId 
+                }
+                models.Game.create(game);
+            }
+            else{
+                return new Promise(resolve => {
+                    resolve({error:true,conflicts: conflicts});
+                });
+            }
+        }
     }
     return game
 }
