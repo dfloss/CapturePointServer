0cb83e17708db4645a834b2bba6446f51c056f0f
Daniel Harris <dgharri@users.noreply.github.com>
2017-07-13 20:59:28 -0500

    controller update

--- app/controller/game.js
+++ app/controller/game.js
@@ -1,6 +1,6 @@
 module.exports = function (events, models, config){
     var game = {
-        start: (name,start,end,teamId) =>{
+        create: (name,start,end,teamId) =>{
             conflicts = models.Game.getConflicts(starttime,endTime);
             if (conflicts == null){
                 var game = {
@@ -9,13 +9,34 @@ module.exports = function (events, models, config){
                     end: end,
                     teamId: teamId 
                 }
-                models.Game.create(game);
+                return models.Game.create(game);
             }
             else{
                 return new Promise(resolve => {
                     resolve({error:true,conflicts: conflicts});
                 });
             }
+        },
+        update: (game)=>{
+            gameId = game.id;
+            var result = delete game.id;
+            return models.Game.update({game},{where: {id: gameId}});
+        },
+        delete: (game) =>{
+            return models.Game.findById(game.id).then(gameRecord =>{
+                return gameRecord.destroy();
+            });
+        },
+        get: (filter) =>{
+            if (filter == null){
+                return models.Game.findAll();
+            }
+            else{
+                return models.Game.findAll(filter);
+            }
+        },
+        getById: (id) =>{
+            return models.Game.findById(id);
         }
     }
     return game
