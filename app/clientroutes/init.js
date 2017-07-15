6097ea1b9a5b7721bbe4dc5b62eb44ec03ea6d27
Charles Kvamme <charliekvamme@gmail.com>
2017-07-14 21:52:51 -0500

    Super sloppy time update
    Time updates now (works fine on pi, doesn't crash on others), not sure
    what to do for redirect though.

--- app/clientroutes/init.js
+++ app/clientroutes/init.js
@@ -1,13 +1,25 @@
+var sys = require('util');
+var exec = require('child_process').exec;
+
 module.exports = function(router, models, arp, config){
     
     router.post('/init', function(req, res){
         var time = req.body.time
-        console.log(req.body);
+        //console.log(req.body);
         console.log(`Init Called with: ${time}`);
         //this is how it should be done eventually
         //controller.setSystemTime(time);
 
 
         //TEST CODE HERE UNTIL CONTROLLER IS READY
+
+        var cmd = "sudo date -s \'" + time + "\'";
+        exec(cmd, function (error, stdout, stderr) {
+          console.log(stdout + stderr);
+          if (error !== null) {
+            console.log('exec error: ' + error);
+          }
+        });
+
     })
 }
