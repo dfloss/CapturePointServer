5f75c913ebcf730c5d6ed893532fa670629c55a2
Daniel Harris <dgharri@users.noreply.github.com>
2017-07-15 13:12:16 -0500

    Controller updates, added response to init.js route

--- scripts/tests/gameModel.tests.js
+++ scripts/tests/gameModel.tests.js
@@ -1,11 +1,13 @@
 var models = require("../../app/models");
 var time = new Date();
 time = time.setMonth(1);
-models.Game.getConflicts(time).then(value =>{
+models.Game.getConflicts(time,null).then(value =>{
     if (value == null){
         console.log("weow");
     }
     else{
-        console.log(value[0].dataValues);
+        value.forEach(function(element) {
+            console.log(element.dataValues);
+        }, this);
     }
 });
